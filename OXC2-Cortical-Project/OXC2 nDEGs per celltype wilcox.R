7ff17b2722b367d9ee63c202b868f73755079242
joachsob <joachsob@stud.ntnu.no>
2025-06-19 16:34:15 +0200

    Finished DEG-script to be functional

--- OXC2-Cortical-Project/OXC2 nDEGs per celltype wilcox.R
+++ OXC2-Cortical-Project/OXC2 nDEGs per celltype wilcox.R
@@ -1,4 +1,7 @@
-count_DEGs_wilcox <- function(seuratObj, celltype_list, condition_pairs, condition_shortnames, 
+# seuratObj <- PrepSCTFindMarkers(seuratObj)
+
+
+fcount_DEGs_wilcox <- function(seuratObj, celltype_list, condition_pairs, condition_shortnames, 
                               group_by1, group_by2, assay = "RNA") {
   
   gene_counts <- list()  # Store DEG counts per cell type
@@ -19,7 +22,7 @@ count_DEGs_wilcox <- function(seuratObj, celltype_list, condition_pairs, conditi
         ident.1 = condition1, ident.2 = condition2, min.pct = 0.2
       )
       
-      degs_df <- degs_df[, c("gene", "avg_log2FC", "logCPM", "P.Value", "p_val_adj")]
+      degs_df <- degs_df[, c("p_val", "avg_log2FC", "pct.1", "pct.2", "p_val_adj")]
       
       # Filter DEGs by log2 fold change > 1
       degs_filtered <- subset(degs_df, abs(avg_log2FC) > 1 & p_val_adj < 0.05)
@@ -44,42 +47,140 @@ count_DEGs_wilcox <- function(seuratObj, celltype_list, condition_pairs, conditi
 
 
 celltype_list <- list(
-  list(full_name = "Arterial ECs", short_name = "Arterial ECs"),
-  list(full_name = "Atrial ECs", short_name = "Atrial ECs"),
-  list(full_name = "Ventricular Cardiomyocytes", short_name = "Ventricular Cardiomyocytes"),
-  list(full_name = "Macrophages", short_name = "Macrophages"),
-  list(full_name = "Cardiac Fibroblasts", short_name = "Cardiac Fibroblasts"),
-  list(full_name = "Schwann Cells", short_name = "Schwann Cells"),
-  list(full_name = "Pericytes", short_name = "Pericytes"),
-  list(full_name = "T-Lymphocytes", short_name = "T-Lymphocytes"),
-  list(full_name = "ECM-High Cardiac Fibroblasts", short_name = "ECM-High Cardiac Fibroblasts"),
-  list(full_name = "Myofibroblasts", short_name = "Myofibroblasts"),
-  list(full_name = "mt-High Ventricular Cardiomyocytes", short_name = "mt-High Ventricular Cardiomyocytes"),
-  list(full_name = "Cardiac Neurons", short_name = "Cardiac Neurons"),
-  list(full_name = "Capillary ECs", short_name = "Capillary ECs"),
-  list(full_name = "Lymphatic ECs", short_name = "Lymphatic ECs"),
-  list(full_name = "Vascular Smooth Muscle Cells", short_name = "VSMC")
+  list(full_name = "vRG", short_name = "vRG"),
+  list(full_name = "Granule Cells", short_name = "Granule Cells"),
+  list(full_name = "NPCs", short_name = "NPCs"),
+  list(full_name = "Proliferating Progenitors", short_name = "Proliferating Progenitors"),
+  list(full_name = "SORCS1+ Immature Ex. Neurons", short_name = "SORCS1+ Immature Ex. Neurons"),
+  list(full_name = "GAD1GAD2+ Granule Cells", short_name = "GAD1GAD2+ Granule Cells"),
+  list(full_name = "oRG", short_name = "oRG"),
+  list(full_name = "Neuroblasts", short_name = "Neuroblasts"),
+  list(full_name = "ARPP21+ Immature Ex. Neurons", short_name = "ARPP21+ Immature Ex. Neurons"),
+  list(full_name = "RELNGAD2+ Inh. Neurons", short_name = "RELNGAD2+ Inh. Neurons"),
+  list(full_name = "Migratory Granule Cells", short_name = "Migratory Granule Cells"),
+  list(full_name = "GPC5GAD2+ Inh. Neurons", short_name = "GPC5GAD2+ Inh. Neurons"),
+  list(full_name = "Preplate Neurons", short_name = "Preplate Neurons"),
+  list(full_name = "ATP1A2+ Fibroblast-Like", short_name = "ATP1A2+ Fibroblast-Like")
 )
 
 unique(seuratObj$celltype)
 
 # Define condition pairs for differential expression analysis
 condition_pairs <- list(
-  c("Patient", "Control")
+  c("4-OXC2-DMSO-control", "11-WTC-control")
 )
 
 # Define short names for conditions
 condition_shortnames <- list(
-  "Control" = "CTRL",
-  "Patient" = "PTNT"
+  "OXC" = "OXC",
+  "WTC" = "WTC"
 )
 
+number_degs <- fcount_DEGs_wilcox(seuratObj, 
+                                 celltype_list = celltype_list, 
+                                 condition_pairs = condition_pairs, 
+                                 condition_shortnames = condition_shortnames, 
+                                 group_by1 = 'celltype', group_by2 = 'sample',
+                                 assay = 'RNA')
+
+number_degs +
+  geom_bar(stat = "identity", fill = "#6BAED6") +  # Nature-style muted blue
+  coord_flip() +
+  labs(title = "Number of DEGs per Cell Type", x = "Cell Type", y = "DEG Count") +
+  theme_minimal(base_size = 12) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text.y = element_text(size = 10),
+    axis.text.x = element_text(size = 10),
+    plot.title = element_text(size = 14, face = "bold")
+  )
+
+# Creating an arbitrary markers list to check column names for adjusting the DEG function
+# testMarkers <- FindMarkers(seuratObj, group.by = 'celltype', ident.1 = 'vRG')
+
+#### Actually correct code ####
+
+count_DEGs_wilcox <- function(seuratObj, celltype_list, condition_pairs, condition_shortnames, 
+                              group_by1, group_by2, assay = "RNA") {
+  
+  gene_counts <- list()  # Store DEG counts per cell type
+  
+  for (celltype in celltype_list) {
+    full_name <- celltype$full_name
+    short_name <- celltype$short_name
+    
+    celltype_subset <- subset(seuratObj, cells = which(seuratObj[[group_by1]] == full_name))
+    
+    for (condition_pair in condition_pairs) {
+      condition1 <- condition_pair[[1]]
+      condition2 <- condition_pair[[2]]
+      
+      seuratObj_sub <- celltype_subset
+      degs_df <- FindMarkers(
+        celltype_subset, group.by = group_by2, assay = assay, verbose = TRUE,
+        ident.1 = condition1, ident.2 = condition2, min.pct = 0.2
+      )
+      
+      degs_df <- degs_df[, c("p_val", "avg_log2FC", "pct.1", "pct.2", "p_val_adj")]
+      
+      # Filter DEGs by log2 fold change > 1
+      degs_filtered <- subset(degs_df, abs(avg_log2FC) > 1 & p_val_adj < 0.05)
+      
+      # Store the count of filtered DEGs
+      gene_counts[[short_name]] <- nrow(degs_filtered)
+    }
+  }
+  
+  # Convert the list to a data frame for plotting
+  gene_counts_df <- data.frame(CellType = names(gene_counts), DEG_Count = unlist(gene_counts))
+  
+  # Create the bar chart
+  number_degs <- ggplot(gene_counts_df, aes(x = reorder(CellType, DEG_Count), y = DEG_Count)) +
+    geom_bar(stat = "identity", fill = "steelblue") +
+    coord_flip() +
+    labs(title = "Number of DEGs per Cell Type", x = "Cell Type", y = "DEG Count") +
+    theme_minimal()
+  
+  return(number_degs)
+}
+
+
+celltype_list <- list(
+  list(full_name = "vRG", short_name = "vRG"),
+  list(full_name = "Granule Cells", short_name = "Granule Cells"),
+  list(full_name = "NPCs", short_name = "NPCs"),
+  list(full_name = "Proliferating Progenitors", short_name = "Proliferating Progenitors"),
+  list(full_name = "SORCS1+ Immature Ex. Neurons", short_name = "SORCS1+ Immature Ex. Neurons"),
+  list(full_name = "GAD1GAD2+ Granule Cells", short_name = "GAD1GAD2+ Granule Cells"),
+  list(full_name = "oRG", short_name = "oRG"),
+  list(full_name = "Neuroblasts", short_name = "Neuroblasts"),
+  list(full_name = "ARPP21+ Immature Ex. Neurons", short_name = "ARPP21+ Immature Ex. Neurons"),
+  list(full_name = "RELNGAD2+ Inh. Neurons", short_name = "RELNGAD2+ Inh. Neurons"),
+  list(full_name = "Migratory Granule Cells", short_name = "Migratory Granule Cells"),
+  list(full_name = "GPC5GAD2+ Inh. Neurons", short_name = "GPC5GAD2+ Inh. Neurons"),
+  list(full_name = "Preplate Neurons", short_name = "Preplate Neurons"),
+  list(full_name = "ATP1A2+ Fibroblast-Like", short_name = "ATP1A2+ Fibroblast-Like")
+)
+
+unique(seuratObj$celltype)
+
+# Define condition pairs for differential expression analysis
+condition_pairs <- list(
+  c("4-OXC2-DMSO-control", "11-WTC-control")
+)
+
+# Define short names for conditions
+condition_shortnames <- list(
+  "OXC" = "OXC",
+  "WTC" = "WTC"
+)
 
 number_degs <- count_DEGs_wilcox(seuratObj, 
                                  celltype_list = celltype_list, 
                                  condition_pairs = condition_pairs, 
                                  condition_shortnames = condition_shortnames, 
-                                 group_by1 = 'celltype', group_by2 = 'condition',
+                                 group_by1 = 'celltype', group_by2 = 'sample',
                                  assay = 'RNA')
 
 number_degs +
