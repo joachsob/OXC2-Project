7101731f5fc46a7ffafd56c32c9229e1726402c9
joachsob <joachsob@stud.ntnu.no>
2025-06-25 15:46:35 +0200

    Created permutation for OXC2

--- OXC2-Cortical-Project/OXC2 Permutation.R
+++ OXC2-Cortical-Project/OXC2 Permutation.R
@@ -3,8 +3,8 @@ library("scProportionTest")
 prop_test <- sc_utils(seuratObj)
 
 prop_test <- permutation_test(
-  prop_test, cluster_identity = "sctype_classification",
-  sample_1 = "Control", sample_2 = "Patient",
-  sample_identity = "condition"
+  prop_test, cluster_identity = "celltype",
+  sample_1 = "WTC", sample_2 = "OXC2",
+  sample_identity = "group"
 )
 permutation_plot(prop_test)
