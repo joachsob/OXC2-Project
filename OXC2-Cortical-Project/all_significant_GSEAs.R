15c9be8b825f371b930a58e9c508f79397526e24
joachsob <joachsob@stud.ntnu.no>
2025-07-08 16:21:14 +0200

    Added code to avoid errors if there are any problems in the p.adjust column of the GSEA results. Added code for collecting the most recurring genes throughout the GSEA results

--- OXC2-Cortical-Project/all_significant_GSEAs.R
+++ OXC2-Cortical-Project/all_significant_GSEAs.R
@@ -1,12 +1,29 @@
+library(readxl)
+library(dplyr)
+library(purrr)
+library(writexl)
+library(tidyr)
+library(stringr)
 
 
-significantGSEAs <- list.files(path = "./GSEA results/WTC Combined vs WTC DMSO/AllSignificantGSEAs", 
+path = "./GSEA results/OXC2 Combined vs OXC2 Control/allSignificantResults/"
+significantGSEAs <- list.files(path = path, 
                                pattern = "\\.xlsx$", full.names = TRUE)
 
 
 # function to read and filter sheets in excel files
 read_and_filter_sheet <- function(file, sheet) {
   df <- read_excel(file, sheet = sheet)
+  
+  print(paste("Reading:", basename(file), "| Sheet:", sheet))
+  print(colnames(df))
+  print(str(df$p.adjust))
+  
+  # If p.adjust is not numeric, convert or skip
+  if (!"p.adjust" %in% colnames(df) || !is.numeric(df$p.adjust)) {
+    message("Skipping: p.adjust missing or not numeric")
+    return(tibble())  # Return empty tibble
+  }
   
   # filter the results by adjusted p-value and selected columns
   df_filtered <- df %>%
@@ -39,6 +56,24 @@ all_results_named <- map2_dfr(filtered_results, names(filtered_results), functio
   })
 })
 
-write_xlsx(all_results_named, "filtered_GSEA_results.xlsx")
+write_xlsx(all_results_named, file.path(path, "OXC2combined_OXC2DMSO_filteredGSEA.xlsx"))
 
-head(all_results_named)
+#### Create a list of the most recurring genes throughout the GSEA results ####
+
+# select and split gene list
+all_genes <- all_results_named %>% 
+  select(core_enrichment) %>%                                     # select gene column
+  filter(!is.na(core_enrichment)) %>%                             # Remove NAs
+  mutate(core_enrichment = str_split(core_enrichment, "/")) %>%   # Split gene list into vectors
+  unnest(core_enrichment) %>%                                     # Flatten the list into rows
+  rename(Gene = core_enrichment)
+
+gene_counts <- all_genes %>%
+  group_by(Gene) %>%
+  summarise(Appearances = n()) %>%
+  arrange(desc(Appearances))
+
+recurrent_genes <- gene_counts %>%
+  filter(Appearances > 1)
+
+write_xlsx(recurrent_genes, file.path(path, "OXC2combined_OXC2DMSO_recurrent genes.xlsx"))
