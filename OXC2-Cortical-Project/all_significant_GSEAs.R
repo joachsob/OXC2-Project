744d3b7ef09a7fc0395f5128908d0f660094ff56
joachsob <joachsob@stud.ntnu.no>
2025-07-10 15:12:18 +0200

    Adjusted gsea code to create one sheet per celltype. Created corresponding code for DEG-filtering

--- OXC2-Cortical-Project/all_significant_GSEAs.R
+++ OXC2-Cortical-Project/all_significant_GSEAs.R
@@ -6,9 +6,15 @@ library(tidyr)
 library(stringr)
 
 
-path = "./GSEA results/OXC2 Combined vs OXC2 Control/allSignificantResults/"
-significantGSEAs <- list.files(path = path, 
-                               pattern = "\\.xlsx$", full.names = TRUE)
+path = "./GSEA results/WTC Miglustat vs WTC DMSO/"          # Path for GSEA results
+gsea_output_file = "WTCmiglustat_WTCDMSO_filteredGSEA.xlsx"  # Desired file for all significant GSEA results
+recurrent_genes_output_file = "WTCmiglustat_WTCDMSO_recurrent genes.xlsx" #Desired file for recurring genes
+
+# Finding all GSEA files in the celltype sub-folders
+all_excel_files <- list.files(path = path, pattern = "\\.xlsx$", full.names =  TRUE, recursive = TRUE)
+gsea_files <- all_excel_files[grepl("GSEA", basename(all_excel_files), ignore.case = TRUE)]
+cell_types <- basename(dirname(gsea_files))
+names(cell_types) <- basename(gsea_files)
 
 
 # function to read and filter sheets in excel files
@@ -34,28 +40,33 @@ read_and_filter_sheet <- function(file, sheet) {
 }
 
 # use the filter function to extract the significant results 
-filtered_results <- map(significantGSEAs, function(file) {
+filtered_results <- map(gsea_files, function(file) {
   sheets <- excel_sheets(file) # get names of all sheets in the Excel file
   sheet_data <- map(sheets, ~ read_and_filter_sheet(file, .x)) # apply filter function to each sheet
   names(sheet_data) <- sheets # name each element in the list by its sheet name
   return(sheet_data)
 })
-names(filtered_results) <- basename(significantGSEAs) # name each top-level list item by the file name
+names(filtered_results) <- basename(gsea_files) # name each top-level list item by the file name
 
 
 # combine every data frame (excel sheet) into one
-# containing every significant result from GSEA
+  # containing every significant result from GSEA
 all_results_named <- map2_dfr(filtered_results, names(filtered_results), function(sheet_list, file_name) {
   map2_dfr(sheet_list, names(sheet_list), function(df, sheet_name) {
     if (nrow(df) > 0) {
       df %>%
-        mutate(Celltype = file_name, Source = sheet_name) %>%
+        mutate(
+          Celltype = cell_types[[file_name]], 
+          Source = sheet_name
+          ) %>%
         relocate(Celltype, Source)  
     }
   })
 })
 
-write_xlsx(all_results_named, file.path(path, "OXC2combined_OXC2DMSO_filteredGSEA.xlsx"))
+# write all significant GSEA results to an Excel file in desired path/file
+write_xlsx(all_results_named, file.path(path, gsea_output_file))
+
 
 #### Create a list of the most recurring genes throughout the GSEA results ####
 
@@ -75,4 +86,4 @@ gene_counts <- all_genes %>%
 recurrent_genes <- gene_counts %>%
   filter(Appearances > 1)
 
-write_xlsx(recurrent_genes, file.path(path, "OXC2combined_OXC2DMSO_recurrent genes.xlsx"))
+write_xlsx(recurrent_genes, file.path(path, recurrent_genes_output_file))
